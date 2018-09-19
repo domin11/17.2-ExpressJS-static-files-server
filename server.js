ac20cbe747f514eefa700c05257709371930eb6a
Dominik Soboń <dominik.sobon@gmail.com>
2018-09-19 15:08:48 +0200

    Add bassic files

--- server.js
+++ server.js
@@ -1,24 +1,13 @@
-var bodyParser = require('body-parser');
 var express = require('express');
-var fs = require('fs');
 var app = express();
-var stringifyFile = "";
 
-app.use(bodyParser.json());
-
-app.get('/getNote', function (req, res) {
-    fs.readFile('./test.json', 'utf8', function(err, data) {
-      if (err) throw err;
-      stringifyFile = data
-      res.send(data);
-    });
+app.get('/', function (req, res) {
+    res.send('Hello world');
 });
 
-app.post('/updateNote/:note', function (req, res) {
-    fs.writeFile('./test.json', stringifyFile + req.params.note, function(err) {
-      if (err) throw err;
-      console.log('file updated');
-    });
-});
+var server = app.listen(3000, 'localhost', function() {
+    var host = server.address().address;
+    var port = server.address().port;
 
-app.listen(3000);
+    console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
+});
